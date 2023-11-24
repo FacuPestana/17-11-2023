27e6062c9b282da4c4aa57a296fa73a7e9837513
alumno <alumno@Instituto.local>
2023-11-24 08:58:41 -0300

    17/11/2023
    Lo utilize para la clase de hoy (DOM), clase 24/11/23

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
 let titulo = document.querySelector('h1')
 let parrafo = document.querySelector('p')
 let button = document.querySelector('button')
-let size 
+let size = document.querySelector ('Parrafo')
 
-button.onclick = function(){
-    size = parrafo.style.fontSize
-    titulo.innerText = 'Soy Facu' 
-    parrafo.style.color = '#24FFEF'
+
+document.querySelector; 'cambiarpropiedadestexto'
+button.onclick = function cambiarTxt(){
+   parrafo.style.color = '#24FFEF' 
     parrafo.style.fontSize = size + 1
 }
+
+ 
